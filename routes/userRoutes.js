23d849d5592ad7dca064b64cc611cfdde48b2985
jthoeun <johnny.thoeun@gmail.com>
2025-03-01 00:37:52 -0700

    W8 updates on swagger

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,7 @@ const userController = require('../controllers/userController');
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 require("dotenv").config();
+const { authenticateJWT } = require('../middleware/authMiddleware'); // OAuth middleware
 
 // Middleware to validate POST/PUT data
 const validateUserData = (req, res, next) => {
@@ -14,22 +15,31 @@ const validateUserData = (req, res, next) => {
   next();
 };
 
-// GET all users
-router.get('/', userController.getAll);
+// GET all users 
+router.get('/', authenticateJWT, userController.getAll);
 
-// GET a single user by username
-router.get('/:username', userController.getUser);
+// GET a single user by username 
+router.get('/:username', authenticateJWT, userController.getUser);
 
-// POST a new user (register)
+// POST a new user (register) - Public
 router.post('/', validateUserData, userController.create);
 
-// PUT to update a user by username
-router.put('/:username', validateUserData, userController.updateUser);
+// PUT to update a user by username 
+router.put('/:username', authenticateJWT, validateUserData, userController.updateUser);
 
 // DELETE a user by username
-router.delete('/:username', userController.deleteUser);
+router.delete('/:username', authenticateJWT, userController.deleteUser);
 
-// POST login route
+// POST login route - Public
 router.post('/login', userController.loginUser);
+
+// Google OAuth Login (Public)
+router.get('/auth/google', userController.googleLogin);
+
+// Google OAuth Callback (Public)
+router.get('/auth/google/callback', userController.googleCallback);
+
+// Logout and invalidate JWT token 
+router.get('/auth/logout', authenticateJWT, userController.logout);
 
 module.exports = router;
